509e4f761b325848e5d2ec489be04dfb85c8d42b
Ihor <ihorhnatchuk@gmail.com>
2020-12-25 15:20:36 +0200

    fixed composer v2

--- src/LKClient.php
+++ src/LKClient.php
@@ -11,10 +11,9 @@ class LKClient
         // $client->param('sub_id_5', '123'); // you can send any params
         // $client->keyword('PASTE_KEYWORD'); // send custom keyword
         // $client->currentPageAsReferrer(); // to send current page URL as click referrer
-        // $client->debug();       // to enable debug mode and show the errors
+        //$client->debug();       // to enable debug mode and show the errors
         // $client->execute();       // request to api, show the output and continue
         //$client->();   // to stop page execution if there is redirect or some output
         return $client;
     }
-	
 }
