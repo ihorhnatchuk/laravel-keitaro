3d4c4636652e4b613aed89e6f1a0b4a98128561f
Ihor Hnatchuk <ihorhnatchuk@gmail.com>
2021-06-10 21:29:33 +0300

    fixed KeitaroClient

--- src/KeitaroClient.php
+++ src/KeitaroClient.php
@@ -5,15 +5,6 @@ class KeitaroClient
 {
     public function execute(string $trackerUrl, string $token)
     {
-        $client = new KClient($trackerUrl, $token);
-        $client->sendAllParams();    // to send all params from page query
-        $client->forceRedirectOffer();    // redirect to offer if an offer is chosen
-        // $client->param('sub_id_5', '123'); // you can send any params
-        // $client->keyword('PASTE_KEYWORD'); // send custom keyword
-        // $client->currentPageAsReferrer(); // to send current page URL as click referrer
-        //$client->debug();       // to enable debug mode and show the errors
-        // $client->execute();       // request to api, show the output and continue
-        //$client->();   // to stop page execution if there is redirect or some output
-        return $client;
+        return new KClient($trackerUrl, $token);
     }
 }
